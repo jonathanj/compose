146cf90f0948d58e7bf7faf6338a5bd04851ad45
Jonathan Jacobs <jonathan@jsphere.com>
2015-06-04 15:08:02 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='compose',
+    version='1.0.0',
+    description='No frills function composition',
+    author='Jonathan Jacobs',
+    url='https://github.com/jonathanj/compose',
+    platforms='any',
+    license='MIT',
+    py_modules=['compose'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Software Development :: Libraries',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
